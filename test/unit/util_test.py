200ae66e4d2a64a4b4975b241641e850cf7b88b8
jlcesardev <jlcesardev@gmail.com>
2026-01-19 13:29:25 +0100

    Update util_test.py

--- test/unit/util_test.py
+++ test/unit/util_test.py
@@ -6,6 +6,7 @@ from app import util
 
 @pytest.mark.unit
 class TestUtil(unittest.TestCase):
+
     def test_convert_to_number_correct_param(self):
         self.assertEqual(4, util.convert_to_number("4"))
         self.assertEqual(0, util.convert_to_number("0"))
@@ -22,6 +23,19 @@ class TestUtil(unittest.TestCase):
         self.assertRaises(TypeError, util.convert_to_number, "s")
         self.assertRaises(TypeError, util.convert_to_number, None)
         self.assertRaises(TypeError, util.convert_to_number, object())
+
+    def test_convert_string_float_with_trailing_dot(self):
+        self.assertEqual(4.0, util.convert_to_number("4."))
+
+    def test_convert_string_float_without_leading_zero(self):
+        self.assertEqual(0.5, util.convert_to_number(".5"))
+
+    def test_convert_string_with_spaces(self):
+        self.assertEqual(4, util.convert_to_number(" 4 "))
+
+    def test_convert_string_float_with_spaces(self):
+        self.assertEqual(4.5, util.convert_to_number(" 4.5 "))
+
 
 if __name__ == "__main__":  # pragma: no cover
     unittest.main()
